c81e5cfbec5bb0fba729969509f5feb6e99c6326
alon276-l <l.alonsodiaz@gmail.com>
2025-10-18 14:31:58 +0200

    more api paths

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from fastapi import Body, FastAPI
 from fastapi.params import Body
 from pydantic import BaseModel
 from typing import Optional
-
+from random import randrange
 
 app= FastAPI()
 
@@ -13,24 +13,37 @@ class Post(BaseModel):
     published: bool = True
     rating: Optional[int] = None
 
-    
+my_posts=[{"Title": "title of post 1", "Content": "content of post 1", "id": 1},{"Title": "favorite foods", "Content": "I like pizza", "id": 2}]
+
+def find_post(id):
+    for p in my_posts:
+        if p['id'] == id:
+            return p
 
 # request get method url: "/"
 @app.get("/")
 def read_root():
-    return {"Hello": "Liii"}
+    return {"data":my_posts}
 
+#retrieve all posts
 @app.get("/posts")
 def get_posts():
-    return {"data": "This is a post"}
+    return {"data": my_posts}
 
-@app.post("/createposts")
-def create_posts(new_post: Post):
-    print(new_post.dict())
-    return {"data": new_post} 
+@app.post("/posts")
+def create_posts(post: Post):
+    post_dict = post.dict()
+    post_dict['id'] = randrange(0,1000000)
+    my_posts.append(post_dict)  
+    return {"data": post_dict} 
     # return {'new_post': f'This is a new post with title {payload["Title"]} and content {payload["Content"]}'}
 
-
+#retrieve a specific post
+@app.get("/posts/{id}")
+def get_post(id):
+    print(id)
+    return {"post_detail": f"Here is post number {id}"}
+    
 
 
 
