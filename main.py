a3a498614994d948883301d8456ccca379d773cf
alon276-l <l.alonsodiaz@gmail.com>
2025-10-19 14:17:17 +0200

    paths added

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 
-from fastapi import Body, FastAPI
+from fastapi import Body, FastAPI, Response, status, HTTPException  
 from fastapi.params import Body
 from pydantic import BaseModel
 from typing import Optional
@@ -19,6 +19,11 @@ def find_post(id):
     for p in my_posts:
         if p['id'] == id:
             return p
+        
+def find_index_post(id):
+    for i,p in enumerate(my_posts):
+        if p['id'] == id:
+            return i
 
 # request get method url: "/"
 @app.get("/")
@@ -30,7 +35,7 @@ def read_root():
 def get_posts():
     return {"data": my_posts}
 
-@app.post("/posts")
+@app.post("/posts",status_code=status.HTTP_201_CREATED)
 def create_posts(post: Post):
     post_dict = post.dict()
     post_dict['id'] = randrange(0,1000000)
@@ -38,14 +43,34 @@ def create_posts(post: Post):
     return {"data": post_dict} 
     # return {'new_post': f'This is a new post with title {payload["Title"]} and content {payload["Content"]}'}
 
+#retrieve last post
+@app.get("/posts/latest")
+def get_latest_post():
+    post= my_posts[len(my_posts)-1] 
+    return {"latest_post": post}    
+
 #retrieve a specific post
-@app.get("/posts/{id}")
-def get_post(id):
-    print(id)
-    return {"post_detail": f"Here is post number {id}"}
+@app.get("/posts/{id:int}")
+def get_post(id,response: Response):
+    post= find_post(id)
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"post with id: {id} was not found") 
+        #response.status_code = status.HTTP_404_NOT_FOUND
+        #return {"message": f"post with id: {id} was not found"}
+    return post
     
 
-
+@app.delete("/posts/{id}")
+def delete_post(id: int):
+    # find the index in the array that has the required id
+    #my_posts.pop(index)
+    index= find_index_post(id)
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"post with id: {id} was not found")             
+    my_posts.pop(index)
+    return {"message": f"post with id: {id} was deleted successfully"}  
 
 
 
